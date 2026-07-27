dee53dba1039870c9683d5fa477fa2ca824d034d
Hariom <hariom80521@gmail.com>
2026-07-27 11:08:44 +0530

    Hariom main.jsx  updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,9 @@ import App from './App.jsx'
 import AuthProvider from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
+  <StrictMode>
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+</StrictMode>
 );
