5e522fa2a033cff3fd00fae18e445fb1eb150c93
Hariom <hariom80521@gmail.com>
2026-07-30 09:32:19 +0530

    updated AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -11,6 +11,7 @@ import Home from "../pages/landing/Home";
 import About from "../pages/landing/About";
 import Contact from "../pages/landing/Contact";
 import HowItWorks from "../pages/landing/HowItWorks";
+import DashboardLayout from "../layouts/DashboardLayout"
 
 // Authentication Pages
 import Login from "../pages/auth/Login";
