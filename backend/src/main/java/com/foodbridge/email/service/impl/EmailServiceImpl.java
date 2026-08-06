24bf711f2bec47058dd5163611d8dbaa3b863ca4
Anushka15-mathur <anushka.mathur.cmfeb26@gmail.com>
2026-08-06 13:22:38 +0530

    feat: enhance authentication, notifications

--- backend/src/main/java/com/foodbridge/email/service/impl/EmailServiceImpl.java
+++ backend/src/main/java/com/foodbridge/email/service/impl/EmailServiceImpl.java
@@ -16,13 +16,7 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendOtpEmail(String to, String name, String otp) {
-
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setTo(to);
-
-        message.setSubject("FoodBridge Password Reset OTP");
-
+        String subject = "FoodBridge Password Reset OTP";
         String body = """
                 Hello %s,
 
@@ -40,8 +34,72 @@ public class EmailServiceImpl implements EmailService {
                 FoodBridge Team
                 """.formatted(name, otp);
 
-        message.setText(body);
+        sendEmail(to, subject, body);
+    }
 
+    @Override
+    public void sendApprovalEmail(String to, String name) {
+        String subject = "FoodBridge Registration Approved";
+        String body = """
+                Hello %s,
+
+                Congratulations!
+
+                Your FoodBridge account has been approved.
+
+                You can now login and start using FoodBridge.
+
+                Thank you for joining our community.
+
+                Regards,
+                FoodBridge Team
+                """.formatted(name);
+
+        sendEmail(to, subject, body);
+    }
+
+    @Override
+    public void sendRejectionEmail(String to, String name) {
+        String subject = "FoodBridge Registration Update";
+        String body = """
+                Hello %s,
+
+                Thank you for registering with FoodBridge.
+
+                Unfortunately, your registration could not be approved.
+
+                Please review your submitted information and register again if required.
+
+                Regards,
+                FoodBridge Team
+                """.formatted(name);
+
+        sendEmail(to, subject, body);
+    }
+
+    @Override
+    public void sendSuspensionEmail(String to, String name) {
+        String subject = "FoodBridge Account Suspended";
+        String body = """
+                Hello %s,
+
+                Your FoodBridge account has been suspended.
+
+                If you believe this action was taken in error,
+                please contact the administrator.
+
+                Regards,
+                FoodBridge Team
+                """.formatted(name);
+
+        sendEmail(to, subject, body);
+    }
+
+    private void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
         mailSender.send(message);
     }
 }
